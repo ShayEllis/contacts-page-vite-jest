accfce2b34426187eae7462559856c1c4a454fd1
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2022-12-25 08:09:25 -0800

    started ContactForm component.

--- src/routes/contactsPage/ContactsPage.jsx
+++ src/routes/contactsPage/ContactsPage.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import ContactForm from '../../components/contactForm/ContactForm';
 
 export const ContactsPage = () => {
+  // Two props - current list of contacts
+  //           - callback function for adding new contact
+
   /*
   Define state variables for 
   contact info and duplicate check
+  - these variables will hold what is entered into the form
   */
+  const [name, setName] = useState([]); // Array of objects
+  const [phone, setPhone] = useState([]); // Array of objects
+  const [email, setEmail] = useState([]); // Array of objects
+
+  //check for duplicate name whenever name changes in the form
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,7 +32,12 @@ export const ContactsPage = () => {
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <ContactForm 
+          name={name}
+          phone={phone}
+          email={email}
+          handleSubmit={handleSubmit} />
       </section>
       <hr />
       <section>
