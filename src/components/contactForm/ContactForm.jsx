c05abd51465f2cf5920aba196a666aa5122b9f10
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2022-12-28 11:18:46 -0800

    added regular expressions to ContactForm

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 
 export const ContactForm = (props) => {
-  // {
-  //   name,
-  //   phone, // this is what the defautl arguments were
-  //   email,
-  //   handleChange
-  //   handleSubmit
-  // }
-
-  //add value to input fields
+ 
   return (
     <form onSubmit={props.handleSubmit}>
       {props.isDuplicate && <h3>Duplicate Name</h3>}
       <label htmlFor='name'>Name</label>
-      <input className={props.isDuplicate ? "duplicate" : ""} type='text' id='name' name='name' value={props.name} onChange={props.handleChange} />
+      <input className={props.isDuplicate ? "duplicate" : ""} type='text' id='name' name='name' value={props.name} onChange={props.handleChange} pattern='[a-zA-Z]+' />
       <label htmlFor='phone'>Phone</label>
-      <input type='text' id='phone' name='phone' value={props.phone} onChange={props.handleChange} />
+      <input type='text' id='phone' name='phone' value={props.phone} onChange={props.handleChange} pattern='\d{3}-?\d{3}-?\d{4}' />
       <label htmlFor='email'>Email</label>
-      <input type='text' id='email' name='email' value={props.email} onChange={props.handleChange} />
+      <input type='text' id='email' name='email' value={props.email} onChange={props.handleChange} pattern='.+@.+[.].+' />
       <input type="submit" value="submit" />
     </form>
   );
--- src/components/tileList/TileList.jsx
+++ src/components/tileList/TileList.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+/*
+-Receive one prop:
+  -An array of objects to render as a list
+-Use the array passed via props to iteratively render Tile components, passing each object in the array as a prop to each rendered Tile component
+*/
+
 export const TileList = () => {
   return (
     <div>
